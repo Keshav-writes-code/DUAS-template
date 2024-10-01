48783c5a14dcab4107827b191a9e13e4c8284e74
keshav-writes-code <dhimankeshav201@gmail.com>
2024-10-01 13:17:23 +0530

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
+import svelte from "@astrojs/svelte";
+import icon from "astro-icon";
+
+export default defineConfig({
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    svelte(),
+    icon(),
+  ],
+  site: "https://Keshav-writes-code.github.io",
+  base: "Project Name",
+});
---
+++ unocss.config.ts
@@ -0,0 +1,17 @@
+// uno.config.ts
+import { defineConfig } from 'unocss'
+import { presetUno } from 'unocss'
+import { presetDaisy } from 'unocss-preset-daisy'
+import transformerVariantGroup from '@unocss/transformer-variant-group'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetDaisy({
+      themes: ["dark"],
+    }),
+  ],
+  transformers: [
+    transformerVariantGroup(),
+  ],
+})
