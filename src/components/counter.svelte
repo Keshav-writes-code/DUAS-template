be1496c3b7ec79e88be3de04c2901938d5acb5b4
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-11-25 13:19:34 +0530

    feat: add large btn and color

--- src/components/counter.svelte
+++ src/components/counter.svelte
@@ -2,7 +2,7 @@
   let count = $state(0)
 </script>
 
-<button class="btn" onclick={()=>{ count++ }} >
+<button class="btn btn-lg btn-primary " onclick={()=>{ count++ }} >
   {count}
   <div class="i-tabler:click size-5"></div>
 </button>
