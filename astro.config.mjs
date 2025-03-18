b44e1305793281835442bb45a1f34498793f9b39
Keshav <95571677+Keshav-writes-code@users.noreply.github.com>
2025-03-18 15:51:10 +0530

    refac(daisyui): use the ameinhardt/unocss-preset-daisy preset for unocss for daisyui instead of taiwlind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import svelte from "@astrojs/svelte";
-import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [
-    tailwind(),
     UnoCSS({
       injectReset: true,
-    }), svelte()],
+    }),
+    svelte(),
+  ],
   site: "https://Keshav-writes-code.github.io",
   base: "DUAS-template",
 });
